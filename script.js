9fb26db9f209452ad693b5e4074c83da6b6cecb0
Joao Victor <imashinzo10@gmail.com>
2022-08-13 11:38:02 -0300

    Primeiro push para testes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const btnEnter = document.querySelector(".btn-enter");
-const inputEmail = document.querySelector(".input-email");
-const inputPassword = document.querySelector(".input-password");
+const btnEnter = document.querySelector('.btn-enter');
+const inputEmail = document.querySelector('.input-email');
+const inputPassword = document.querySelector('.input-password');
 
 function loginValid() {
   if (
-    inputEmail.value === "tryber@teste.com" &&
-    inputPassword.value === "123456"
+    inputEmail.value === 'tryber@teste.com'
+    && inputPassword.value === '123456'
   ) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
-btnEnter.addEventListener("click", loginValid);
+btnEnter.addEventListener('click', loginValid);
 
-/* Validacao do botao para o requisito 18, refencia : https://www.w3schools.com/howto/howto_js_display_checkbox_text.asp*/
+/* Validacao do botao para o requisito 18, refencia : https://www.w3schools.com/howto/howto_js_display_checkbox_text.asp */
 
-const checkboxOk = document.getElementById("agreement");
-const ifChecked = document.getElementById("submit-btn");
+const checkboxOk = document.getElementById('agreement');
+const ifChecked = document.getElementById('submit-btn');
 
 function formChecked() {
   if (checkboxOk.checked === true) {
